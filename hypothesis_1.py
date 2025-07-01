2082bfbd01d3d892ab370e6398bc253663d2a83c
Luca Witt <witt-luca@web.de>
2025-07-01 16:02:44 +0200

    Add 1st Hypothesis to hypotheses.org

--- hypothesis_1.py
+++ hypothesis_1.py
@@ -7,20 +7,31 @@ import numpy as np
 import h5py
 import faiss
 
-n_sites = [10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
-n_dims = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
-k = 5
 filepath = "data/hypothesis_1"
 
 np.random.seed(42)
+
+hypothesis = "HNSW remains more precise than LSH on clustered data as the number of sites grows."
+
+description = '''
+We generate two clusters, centered at $(-0.5, \dots, -0.5)$ and $(0.5, \dots, 0.5)$ respectively. These are hypercubes with a diameter of $0.4$. Put differently, for all sites $s = (s_1, \dots, s_d)$ in the first cluster, it holds that $-0.7 \leq s_i \leq -0.3$. For the second cluster this corresponds to $0.3 \leq s_i \leq 0.7$. Note that sites within each cluster are sampled uniformly.
+
+'''
+
+n_sites = [10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
+n_dims = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+k = 5
 
 assert(len(n_sites) == len(n_dims))
 
 file = h5py.File(filepath, 'w')
 file.attrs['k'] = k
 file.attrs['n_instances'] = len(n_sites)
+file.attrs['description'] = description
+file.attrs['hypothesis'] = hypothesis
 
 for i in range(len(n_sites)):
+
     print(f'Generating instance {i}:')
     time_start = time.perf_counter()
     first_cluster = np.random.uniform(-0.7, -0.3, (int(n_sites[i] / 2), n_dims[i]))
@@ -28,7 +39,9 @@ for i in range(len(n_sites)):
     sites = first_cluster + second_cluster
     time_end = time.perf_counter()
     print(f'\tGenerating sites: {time_end - time_start:.3f} seconds')
+
     query = np.random.uniform(-1.0, 1.0, (1, n_dims[i]))
+
     time_start = time.perf_counter()
     index = faiss.IndexFlatL2(n_dims[i])
     index.add(sites)
