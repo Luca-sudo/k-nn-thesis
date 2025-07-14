419920cce9721fce603dd213d42209a20e9a8bb5
Luca Witt <witt-luca@web.de>
2025-07-14 13:21:57 +0200

    Hypothesis 3: Fix Axes & Values

--- hypotheses/hypothesis_3.py
+++ hypotheses/hypothesis_3.py
@@ -10,6 +10,7 @@ import faiss
 n_dims = 2
 k = 5
 extents = [5, 10, 20, 30, 40, 50, 75, 100, 200, 300, 500]
+sample_size = 20
 filepath = "data/hypothesis_3"
 
 hypothesis = "HNSW remains precise on a uniform grid, whereas LSH degenerates due to cosine similarity collisions."
@@ -26,6 +27,9 @@ file.attrs['k'] = k
 file.attrs['n_instances'] = len(extents)
 file.attrs['description'] = description
 file.attrs['hypothesis'] = hypothesis
+file.attrs['sample_size'] = sample_size
+file.attrs['var_name'] = "Extent"
+file.attrs['var_values'] = extents
 
 for i in range(len(extents)):
 
@@ -36,7 +40,7 @@ for i in range(len(extents)):
     time_end = time.perf_counter()
     print(f'\tGenerating sites: {time_end - time_start:.3f} seconds')
 
-    query = np.random.uniform(0, extents[i], (1, n_dims))
+    queries = np.random.uniform(0, extents[i], (sample_size, n_dims))
     time_start = time.perf_counter()
     index = faiss.IndexFlatL2(n_dims)
     index.add(sites)
@@ -44,29 +48,29 @@ for i in range(len(extents)):
     print(f'\tGenerating flat index: {time_end - time_start:.3f} seconds')
 
     time_start = time.perf_counter()
-    distance, solution = index.search(query, extents[i] ** 2)
+    distance, solution = index.search(queries, extents[i] ** 2)
     time_end = time.perf_counter()
     print(f'\tComputing solution: {time_end - time_start:.3f} seconds')
 
-    k_nearest = solution[0][:k]
-    ranks = solution[0]
-    k_nearest_distances = distance[0][:k]
+    k_nearest = list(map(lambda x: x[:k], solution))
+    ranks = solution
+    k_nearest_distances = list(map(lambda x: x[:k], distance))
 
     def invert(l):
         new_l = [0 for i in range(len(l))]
 
         for index, value in enumerate(l):
-            new_l[value] = index
+            new_l[value] = index + 1
 
         return new_l
 
-    ranks = invert(ranks)
+    ranks = list(map(invert, ranks))
 
     instance = file.create_dataset('instance_' + str(i), data=sites)
     instance.attrs['n_sites'] = extents[i] ** 2
     instance.attrs['n_dims'] = n_dims
     instance.attrs['n_planes'] = n_dims * 2
-    file.create_dataset('query_' + str(i), data=query)
+    file.create_dataset('queries_' + str(i), data=queries)
     file.create_dataset('solution_' + str(i), data=k_nearest)
     file.create_dataset('ranks_' + str(i), data = ranks)
     file.create_dataset('distance_' + str(i), data=k_nearest_distances)
