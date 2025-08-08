db00790956316cd0dc8d9f04ca0556dde7172bc9
Luca Witt <witt-luca@web.de>
2025-08-08 11:41:42 +0200

    Refactor hypothesis 5 with tangle-blocks

--- hypotheses/hypothesis_5.py
+++ hypotheses/hypothesis_5.py
@@ -1,17 +1,32 @@
-#!/usr/bin/env python3
-
-# Hypothesis 5: Over a uniform-grid similar to hypotheses 3 & 4, the quality of LSH diminishes as the dimensionality of the feature space increases over a bounded region.
-
 import itertools
+#!/usr/bin/env python3
 import time
 import numpy as np
 import h5py
 import faiss
 
-n_dims = [i for i in range(2, 11)]
-k = 5
+# Given a list that contains the ids of sites, invert populates a new list where the value at the id is the rank of the site.
+# If, for example, the site with id 5 was the nearest neighbor, then the resulting list would have value 1 at position 5.
+# This way, computing the ranks of candidate solutions is possible in constant time, at the cost of some memory.
+def invert(l):
+    new_l = [0 for i in range(len(l))]
+
+    for index, value in enumerate(l):
+        new_l[value] = index + 1
+
+    return new_l
+
+# Hypothesis 5: Over a uniform-grid similar to hypotheses 3 & 4, the quality of LSH diminishes as the dimensionality of the feature space increases over a bounded region.
+
 extent = 5
+n_dims = [i for i in range(2, 10)]
+n_planes = [dim * 2 for dim in n_dims]
+n_sites = [5 ** dim for dim in n_dims]
+k = [5 for i in range(len(n_dims))]
 sample_size = 20
+site_generator = lambda i: [s for s in itertools.product(range(extent), repeat=n_dims[i])]
+query_generator = lambda i: np.random.uniform(0, extent, (sample_size, n_dims[i]))
+
 filepath = "data/hypothesis_5.h5"
 
 hypothesis = "Over a uniform-grid similar to hypotheses 3 & 4, the quality of LSH diminishes as the dimensionality of the feature space increases."
@@ -24,23 +39,23 @@ np.random.seed(42)
 
 file = h5py.File(filepath, 'w')
 file.attrs['k'] = k
-file.attrs['n_instances'] = len(n_dims)
+file.attrs['n_dims'] = n_dims
+file.attrs['n_sites'] = n_sites
+file.attrs['n_planes'] = n_planes
+file.attrs['n_instances'] = len(n_sites)
 file.attrs['description'] = description
 file.attrs['hypothesis'] = hypothesis
 file.attrs['sample_size'] = sample_size
-file.attrs['var_name'] = "Dimension"
-file.attrs['var_values'] = n_dims
 
-for i in range(len(n_dims)):
-
+for i in range(len(n_sites)):
     print(f'Generating instance {i}:')
     time_start = time.perf_counter()
-    sites = [s for s in itertools.product(range(extent), repeat=n_dims[i])]
+    sites = site_generator(i)
     sites = np.array(sites, dtype=np.float32)
+    print(f'sites: {sites}')
     time_end = time.perf_counter()
     print(f'\tGenerating sites: {time_end - time_start:.3f} seconds')
-
-    queries = np.random.uniform(0, extent, (sample_size, n_dims[i]))
+    queries = query_generator(i)
     time_start = time.perf_counter()
     index = faiss.IndexFlatL2(n_dims[i])
     index.add(sites)
@@ -48,30 +63,31 @@ for i in range(len(n_dims)):
     print(f'\tGenerating flat index: {time_end - time_start:.3f} seconds')
 
     time_start = time.perf_counter()
-    distance, solution = index.search(queries, extent ** n_dims[i])
+    distance, solution = index.search(queries, n_sites[i])
+
     time_end = time.perf_counter()
     print(f'\tComputing solution: {time_end - time_start:.3f} seconds')
 
-    k_nearest = list(map(lambda x: x[:k], solution))
+    k_nearest = list(map(lambda x: x[:k[i]], solution))
     ranks = solution
-    k_nearest_distances = list(map(lambda x: x[:k], distance))
-
-    def invert(l):
-        new_l = [0 for i in range(len(l))]
-
-        for index, value in enumerate(l):
-            new_l[value] = index + 1
-
-        return new_l
+    print(ranks[0])
+    print(len(ranks[0]))
 
     ranks = list(map(invert, ranks))
 
+    site_to_distance = []
+    for sample_idx in range(sample_size):
+        temp = [0.0 for i in range(n_sites[i])]
+        for j in range(len(temp)):
+            site_idx = solution[sample_idx][j]
+            temp[site_idx] = distance[sample_idx][j]
+        site_to_distance.append(temp)
 
     instance = file.create_dataset('instance_' + str(i), data=sites)
-    instance.attrs['n_sites'] = extent ** n_dims[i]
-    instance.attrs['n_dims'] = n_dims[i]
-    instance.attrs['n_planes'] = n_dims[i] * 2
     file.create_dataset('queries_' + str(i), data=queries)
     file.create_dataset('solution_' + str(i), data=k_nearest)
     file.create_dataset('ranks_' + str(i), data = ranks)
-    file.create_dataset('distance_' + str(i), data=k_nearest_distances)
+    file.create_dataset('distance_' + str(i), data=site_to_distance)
+
+file.attrs['var_name'] = "Dimension"
+file.attrs['var_values'] = n_dims
